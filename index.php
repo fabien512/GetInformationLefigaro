2aaf0f7f01020358c2fddaa23fdfa91fe388fd52
fabien512 <ghrenassia@eisti.eu>
2014-07-03 12:14:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 	require_once("LeFigaroConnector.php");
 	require_once("Article.php");
+	
+	/**
+	 * Load a test function with a specified day (or specified month) and a keyword. Export the result as .xml file
+	 * 
+	 * */
+	 
 
 	function test() {
 		$connector = new LeFigaroConnector(01,03,2013,'france');
